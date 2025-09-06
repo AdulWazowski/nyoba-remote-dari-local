5a81dfff40a36036f0f1f0a4336277736e2f0dff
AdulWazowski <taufikhan17@gmail.com>
2025-09-07 00:47:43 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coba remote dari local</title>
+</head>
+<body>
+    <h1>awik</h1>
+</body>
+</html>
